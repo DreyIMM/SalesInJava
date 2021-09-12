e0e2e512db8fede00a9aca2ab7383115a67e708d
DreyIMM <andreydario26@gmail.com>
2021-09-11 23:37:54 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hellow World</h1>
+      <h1 className="text-primary">Hellow World</h1>
     </div>
   );
 }
